5e31359fbf68d9e84ea773633a7b37eaa388c4e9
jacobmix <jacobmix@users.noreply.github.com>
2024-06-27 21:08:50 +0200

    Update bot.js to auto connect to server, and post in channel
    This is hacky way. Basically just copied the slash command for this, and slightly changed it. No option to toggle this or set a config for channel id.

--- bot.js
+++ bot.js
@@ -4,6 +4,13 @@ const { cachePartial } = require('./lib');
 const { generalErrorHandler } = require('./errorHandlers');
 const fs = require('fs');
 
+const ArchipelagoInterface = require('./Archipelago/ArchipelagoInterface');
+const { SlashCommandBuilder, User } = require('discord.js');
+const { AsciiTable3, AlignmentEnum } = require('ascii-table3');
+const { ITEMS_HANDLING_FLAGS, COMMON_TAGS, SERVER_PACKET_TYPE, ConnectionStatus } = require('archipelago.js');
+//const { Client, ITEMS_HANDLING_FLAGS, COMMON_TAGS, SERVER_PACKET_TYPE, ConnectionStatus } = require('archipelago.js');
+const { v4: uuid } = require('uuid');
+
 // Catch all unhandled errors
 process.on('uncaughtException', (err) => generalErrorHandler(err));
 process.on('unhandledRejection', (err) => generalErrorHandler(err));
@@ -61,6 +68,101 @@ client.on(Events.Error, async(error) => generalErrorHandler(error));
 client.once(Events.ClientReady, async () => {
   // Login and initial setup successful
   console.info(`Connected to Discord. Active in ${client.guilds.cache.size} guilds.`);
+
+  // run command after startup?
+  const channel = client.channels.cache.get('1236668389796089877');
+
+    //async execute() {
+        serverAddress = null;
+        port = null;
+        slotName = null;
+        password = null;
+
+        if (serverAddress === null) {
+          serverAddress = config.serverAddress ?? "127.0.0.1";    
+        }
+        if (port === null) {
+          port = Number(config.port) ?? 12345;
+        }
+        if (slotName === null) {
+          slotName = config.slotName ?? "no name";
+        }
+        if (password === null) {
+          password = config.password ?? null;
+        }
+
+        if (client.tempData.apInterfaces.has(channel.id)) {
+          return channel.send({
+            content: 'An Archipelago game is already being monitored in this channel ' +
+              'and must be disconnected before a new game can be monitored.',
+            ephemeral: true,
+          });
+        }
+
+        if (serverAddress === 'archipelago.gg') {
+          return channel.send({
+            content: 'This bot connot be used with the official Archipelago server. ' +
+              'Please to a self hosted server.',
+            ephemeral: true,
+          });
+        }
+        
+
+  APInterface = new ArchipelagoInterface(channel, config.serverAddress, Number(config.port), config.slotName, config.password);
+  
+        // Check if the connection was successful every half second for five seconds
+        for (let i=0; i<10; ++i){
+          // Wait half of a second
+          await new Promise((resolve) => (setTimeout(resolve, 500)));
+
+          if (APInterface.APClient.status === 'Connected') {
+            client.tempData.apInterfaces.set(channel.id, APInterface);
+            await channel.send({
+              content: `Connected to ${config.serverAddress} with slot ${config.slotName}.`,
+              ephemeral: false,
+            });
+
+	        // Set the APInterface to show all kinds of messages
+            client.tempData.apInterfaces.get(channel.id).showItems = true;
+            client.tempData.apInterfaces.get(channel.id).showProgression = true;
+            client.tempData.apInterfaces.get(channel.id).showHints = true;
+            client.tempData.apInterfaces.get(channel.id).showChat = true;
+            return channel.send({
+                content: 'Showing item, progression, hint, and chat messages.',
+                ephemeral: false,
+            });
+
+            // TODO: The following does not work. status doesn't seem to be a good way to check 
+            // for disconnects. Replace with auto reconnect behavior based on some pinging method. 
+            // Until then, the following will run forever.
+
+            // Make this run until not longer connected to the AP server.
+	          while (APInterface.APClient.status === 'Connected') {	
+              //console.log("Still connected to server");
+              await new Promise((resolve) => (setTimeout(resolve, 5000)));
+		        }
+
+            await channel.send({
+		          content: `Disconnected from AP server at ${config.serverAddress}.`,
+		          ephemeral: false,
+		        });
+
+            return setTimeout(() => {
+              console.log("Lost connection with AP server.");
+              if (client.tempData.apInterfaces.has(channel.id)) {
+                client.tempData.apInterfaces.get(channel.id).disconnect();
+                client.tempData.apInterfaces.delete(channel.id);
+              }
+            }, 5000);
+          }
+        }
+        
+		// If the client fails to connect, notify the user
+        return interaction.reply({
+          content: `Unable to connect to AP server at ${config.serverAddress}.`,
+          ephemeral: false,
+        });
+	//}
 });
 
 client.login(config.token);
